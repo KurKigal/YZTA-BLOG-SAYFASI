8033bdf34e3f234e28b82a859459a523d33d9789
kadrzeybek <kadrzeybek@gmail.com>
2026-02-01 17:44:58 +0300

    backend first setup

---
+++ backend/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/<int:user_id>/', views.user_profile, name='user_profile'),
+]
